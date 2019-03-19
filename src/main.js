dc8f6b83762728635f2ae7650b1980525b664e45
Ryan McLean and Victoria Martinez <ryan@>
2019-03-19 16:39:13 -0700

    add api key and .env file

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import $ from 'jquery';
 import './input.scss';
 
+var unirest = require('unirest');
+
+
 $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
     $.ajax({
-        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=f90163aedea7c0a2406f9202c878ba2b`,
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.API_KEY}`,
         type: `GET`,
         data: {
           format: 'json'
